ef2a5e52d0aa3f686efcfa69e4e52569ab2fc80c
RousseOrtiz <ri.ortizguzman@ugto.mx>
2022-12-01 17:06:36 -0600

    ruta para el login del cliente

--- back/routes/cliente.js
+++ back/routes/cliente.js
@@ -4,7 +4,8 @@ var express = require('express');
 var clienteController = require('../controllers/ClienteController');
 
 var api = express.Router();
-
+//rutas
 api.post('/registro_cliente',clienteController.registro_cliente);
+api.post('/login_cliente', clienteController.login_cliente);
 
 module.exports = api;
