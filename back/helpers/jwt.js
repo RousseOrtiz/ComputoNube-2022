1dee112861978c6458f990ce85a7cbcf652ab08b
RousseOrtiz <ri.ortizguzman@ugto.mx>
2022-12-01 18:03:43 -0600

    helper jwt para token creado

---
+++ back/helpers/jwt.js
@@ -0,0 +1,19 @@
+//generacion de token
+'use strict'
+
+var jwt = require('jwt-simple'); //para decodificar tokens
+var moment = require('moment');
+var secret = 'rousseort';  //contraseña para encriptar datos
+
+exports.createToken = function(user){
+    var payload = {
+        sub: user._id,
+        nombres: user.nombres,
+        apellidos: user.apellidos,
+        email: user.email,
+        iat: moment().unix(), //fecha que se creo el token
+        exp: moment().add(7,'days').unix() //fecha expiracion del token
+    }
+
+    return jwt.encode(payload,secret);
+}
