590215965e60129eb6b6221ca1b94f03fe4564c9
RousseOrtiz <ri.ortizguzman@ugto.mx>
2022-12-07 17:04:14 -0600

    generacion carpeta para login, creacion de advertencias con iziToast y creacion de carpeta para servicios

--- admin/src/app/app.routing.ts
+++ admin/src/app/app.routing.ts
@@ -2,10 +2,12 @@
 import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
 import { InicioComponent } from "./components/inicio/inicio.component";
+import { LoginComponent } from "./components/login/login.component";
 
 //array con todas las rutas
 const appRoute : Routes = [
-    {path: "", component: InicioComponent}
+    {path: "", component: InicioComponent},
+    {path: 'login', component: LoginComponent} //ruta para el componnete login
 ]
 
 export const appRoutingProviders : any[]=[];
--- admin/src/app/components/login/login.component.ts
+++ admin/src/app/components/login/login.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+declare var jQuer:any;
+declare var $:any;
+declare var iziToast:any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  public user : any = {};
+
+  constructor(){
+
+  }
+  ngOnInit(): void{
+  }
+
+  login(loginForm: { valid: any; }){
+    if(loginForm.valid){
+      console.log(this.user);
+
+      alert('si es valido');
+    }else{
+      iziToast.show({
+      title : 'ERROR',
+      titleColor : '#FF0000',
+      color: 'red',
+      class : 'text-danger',
+      position : 'topRight',
+      message : 'Los datos del formulario no son validos' 
+     });
+    }
+  }
+  
 }
