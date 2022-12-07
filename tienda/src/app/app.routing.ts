643d0462afd923255c3ae54f53ecbc9f60e55d3d
RousseOrtiz <ri.ortizguzman@ugto.mx>
2022-12-07 11:29:13 -0600

    agregado de nueva ruta en app.routng.ts

--- tienda/src/app/app.routing.ts
+++ tienda/src/app/app.routing.ts
@@ -1,10 +1,11 @@
 //enrutado del proyecto
 import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
+import { InicioComponent } from "./components/inicio/inicio.component";
 
 //array con todas las rutas
 const appRoute : Routes = [
-
+    {path: '', component: InicioComponent}
 ]
 
 export const appRoutingProviders : any[]=[];
