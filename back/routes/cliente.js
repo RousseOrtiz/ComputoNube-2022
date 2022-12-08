1a0d43372e8b338ede80fadf9cf141232ef30d03
RousseOrtiz <ri.ortizguzman@ugto.mx>
2022-12-07 23:53:55 -0600

    creacion ruta

--- back/routes/cliente.js
+++ back/routes/cliente.js
@@ -4,8 +4,11 @@ var express = require('express');
 var clienteController = require('../controllers/ClienteController');
 
 var api = express.Router();
-//rutas
+//rutas de accesos
 api.post('/registro_cliente',clienteController.registro_cliente);
 api.post('/login_cliente', clienteController.login_cliente);
+
+api.get('/listar_cientes_filtro_admin', clienteController.listar_clientes_fitro_dmin);
+
 
 module.exports = api;
