c270b28ffc7b75241e357d249f28019fcd3a411e
RousseOrtiz <ri.ortizguzman@ugto.mx>
2022-12-08 20:25:00 -0600

    proteccion de funciones de las rutas del API con tokens, backend

--- back/controllers/ClienteController.js
+++ back/controllers/ClienteController.js
@@ -57,26 +57,34 @@ const login_cliente = async function(req,res){
         
     }
 }
-
+//verificacion de permisos suficientes para acceder a peticion
 const listar_clientes_filtro_admin =async function(req,res){
+    console.log(req.user);
+    if(req.user){
+        if(req.user.role == 'admin'){
+            let tipo = req.params['tipo'];
+            let filtro = req.params['filtro'];
 
-    let tipo = req.params['tipo'];
-    let filtro = req.params['filtro'];
+            console.log(tipo);
 
-    console.log(tipo);
-
-    if (tipo == null || tipo == 'null'){
-        let reg = await Cliente.find();
-        res.status(200).send({data:reg});
-    }else{
-        //Filtro
-        if(tipo == 'apellidos'){
-            let reg = await Cliente.find({apellidos:new RegExp(filtro,'i')});
-            res.status(200).send({data:reg});
-        }else if (tipo == 'correo'){
-            let reg = await Cliente.find({email:new RegExp(filtro,'i')});
-            res.status(200).send({data:reg});
+            if (tipo == null || tipo == 'null'){
+                let reg = await Cliente.find();
+                res.status(200).send({data:reg});
+            }else{
+                //Filtro
+                if(tipo == 'apellidos'){
+                    let reg = await Cliente.find({apellidos:new RegExp(filtro,'i')});
+                    res.status(200).send({data:reg});
+                }else if (tipo == 'correo'){
+                    let reg = await Cliente.find({email:new RegExp(filtro,'i')});
+                    res.status(200).send({data:reg});
+                }
+            }
+        }else{
+            res.status(500).send({message: 'NoAccess'});
         }
+    }else{
+        res.status(500).send({message: 'NoAccess'});
     }
 }
 
