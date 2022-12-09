8597ab6f738349257a5f2c5c4f6a47796877dd8d
RousseOrtiz <ri.ortizguzman@ugto.mx>
2022-12-08 21:50:16 -0600

    Correcion en html del registro cliente y

--- admin/src/app/components/clientes/create-cliente/create-cliente.component.ts
+++ admin/src/app/components/clientes/create-cliente/create-cliente.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var iziToast:any;
 
 @Component({
   selector: 'app-create-cliente',
   templateUrl: './create-cliente.component.html',
   styleUrls: ['./create-cliente.component.css']
 })
-export class CreateClienteComponent {
+export class CreateClienteComponent implements OnInit{
+  public cliente : any ={
+    genero: '' //inicializacion de genero
+  };
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  registro(registroForm: any){
+      if(registroForm.valid){
+        console.log(this.cliente);
+      }else{
+        iziToast.show({
+          title : 'ERROR',
+          titleColor : '#FF0000',
+          color: 'red',
+          class : 'text-danger',
+          position : 'topRight',
+          message : 'Los datos del formulario no son validos' 
+         });
+      }
+  }
 }
